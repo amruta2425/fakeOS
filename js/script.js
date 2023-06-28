6af10d5771c6a592a31760d9c48ccbad06ec639f
amruta2425 <amruta.marathe25@gmail.com>
2023-06-28 21:42:24 +1000

    changed index.html and script.js

--- js/script.js
+++ js/script.js
@@ -1,59 +1,68 @@
-
 let modal = document.getElementById("myModal");
 let closeBtn = document.getElementsByClassName("close")[0];
+
+
+/*-----------------Open Modal-------------*/
 function openModal() {
   modal.style.display = "block";
 }
+
+
+/*-------------------Close Modal------------*/
 function closeModal() {
   modal.style.display = "none";
 }
 
+
+/*----------------Adobe App------------------*/
 let app = document.getElementById("adobeApp");
 app.addEventListener("click", openModal);
 closeBtn.addEventListener("click", closeModal);
 
+
+/*------------------Youtube-----------------*/
 document.addEventListener("DOMContentLoaded", function() {
   let image = document.getElementById("popup-image");
   image.addEventListener("click", function() {
-      
-      let newWindow = window.open("http://www.youtube.com");
+  let newWindow = window.open("http://www.youtube.com");
   });
 });
 
+/*-------------------Chrome-----------------*/
 document.addEventListener("DOMContentLoaded", function() {
   let chrome = document.getElementById("popup-chrome");
   chrome.addEventListener("click", function() {
-      let newWindow1 = window.open("http://www.google.com");
-     
-  });
+  let newWindow1 = window.open("http://www.google.com");
+ });
 });
 
-function formatDate(date) {
-  let hours = date.getHours();
-  let minutes = date.getMinutes();
-  let ampm = hours >= 12 ? 'pm' : 'am';
-  hours = hours % 12;
-  hours = hours ? hours : 12; 
-  minutes = minutes < 10 ? '0'+minutes : minutes;
-  let strTime = hours + ':' + minutes + ' ' + ampm;
-  return strTime;
+
+/*-------------Displaying the Clock-----------*/
+// Update the clock every second
+const clockContainer = document.querySelector("#section__clock--align");
+
+const updateClock = () => {
+  const now = new Date();
+  clockContainer.innerHTML = now.toLocaleTimeString();
 }
 
-const getCurrentDateTime = () => {
-  let currentDateTime = new Date();
-  let year = currentDateTime.getFullYear();
-  const months = ["January", "February", "March", "April", "May", "Jun", "July", "August", "September", "October", "November", "December"];
-  let month = months[currentDateTime.getMonth()];
-  let date = currentDateTime.getDate();
-  const days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
-  let day = days[currentDateTime.getDay()];
-  let formattedDateTime = day+ " "+date+ " "+month+ " "+ formatDate(currentDateTime);
-  document.getElementById("datetime").textContent = formattedDateTime;
-};
-setInterval(getCurrentDateTime, 1000);
+setInterval(updateClock, 1000); 
+
+//To display container for start button
+const menuButton = document.getElementById("section__start-button--align");
+const menu = document.getElementById("section__menu--flex");
+
+menuButton.addEventListener("click", () => {
+  menu.style.display = menu.style.display === "none" ? "flex" : "none";
+});
 
 
+//Display nothing when clicked on the screen
 
+var onTheScreen = document.getElementById("section__background--image");
+onTheScreen.addEventListener("click", event =>{
 
-
-
+  if (event.target === onTheScreen) {
+        menu.style.display = "none";
+      }
+});
